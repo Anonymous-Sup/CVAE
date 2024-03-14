correct kl

models/CVAE.py
@@ -30,6 +30,7 @@ class VAE(nn.Module):
         self.decoder = Decoder(
             decoder_layer_sizes, latent_size)
         
+        self.apply(self.init_weights)
     # remember to be fp16
     def forward(self, x):
         return True
@@ -44,6 +45,11 @@ class VAE(nn.Module):
         recon_x = self.decoder(z, c)
 
         return recon_x
+
+    def init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            nn.init.xavier_uniform_(m.weight)
+            nn.init.constant_(m.bias, 0)
 
 
 class Encoder(nn.Module):
@@ -62,7 +68,6 @@ class Encoder(nn.Module):
             self.MLP.add_module(
                 name="L{:d}".format(i), module=nn.Linear(in_size, out_size))
             self.MLP.add_module(name="A{:d}".format(i), module=nn.ReLU())
-
         self.linear_means = nn.Linear(layer_sizes[-1], latent_size)
         self.linear_log_var = nn.Linear(layer_sizes[-1], latent_size)
 

models/Flows.py
@@ -49,8 +49,8 @@ class Flows(nn.Module):
             b = torch.Tensor([1 if i % 2 == 0 else 0 for i in range(input_dim)])
             flows = []
             for i in range(K):
-                s = nets.MLP([input_dim, 8, input_dim])
-                t = nets.MLP([input_dim, 8, input_dim])
+                s = nets.MLP([input_dim, 8, input_dim], init_zeros=True)
+                t = nets.MLP([input_dim, 8, input_dim], init_zeros=True)
                 if i % 2 == 0:
                     flows += [MaskedAffineFlow(b, t, s)]
                 else:
@@ -62,6 +62,7 @@ class Flows(nn.Module):
             raise ValueError("Invalid flow type: {}".format(flow_type))
         self.flows = flows
 
+
     def forward(self, x):
         theta, logjcobin = self.flows(x)
         return theta, logjcobin
@@ -69,6 +70,7 @@ class Flows(nn.Module):
     def inverse(self, theta):
         z, logjcobin = self.flows.inverse(theta)
         return z, logjcobin
+
     
 
 class InvertibleMLPFlow(nn.Module):

models/NIPS.py
@@ -13,14 +13,19 @@ class NIPS(nn.Module):
         self.domain_embeding.add_module('domain_embeding_layer1', nn.Linear(feature_dim, latent_size))
         self.domain_embeding.add_module('domain_embeding_activate', nn.ReLU())
 
+        
         self.fusion = nn.Sequential()
         self.fusion.add_module('fusion_layer1', nn.Linear(latent_size*2, latent_size))
         self.fusion.add_module('fusion_activate', nn.ReLU())
 
+        self.domain_embeding.apply(self.init_weights)
+        self.fusion.apply(self.init_weights)
 
-    def forward(self, x, domain_index_feat):
+    def forward(self, x, domain_index):
+        
 
-        domain_index_feat= self.domain_embeding(domain_index_feat)
+        domain_index_feat= self.domain_embeding(domain_index)
+        domain_index_feat = self.normalization(domain_index_feat)
 
         means, log_var = self.VAE.encoder(x)
         z_0 = self.VAE.reparameterize(means, log_var)
@@ -33,3 +38,12 @@ class NIPS(nn.Module):
         recon_x = self.VAE.decoder(z_0)
 
         return recon_x, means, log_var, z_0, z_1, theta, logjcobin
+    
+    def init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            nn.init.xavier_uniform_(m.weight)
+            nn.init.constant_(m.bias, 0)
+    
+    def normalization(self, x):
+        x_normalized = (x - torch.min(x)) / (torch.max(x) - torch.min(x))
+        return x_normalized

train.py
@@ -69,7 +69,7 @@ def train_cvae(run, config, model, classifier, criterion_cla, criterion_pair, cr
 
             # kld_theta = (
             #     -torch.sum(prior.log_prob(theta), dim=-1)
-            #     + torch.sum(q0.log_prob(theta), dim=-1)
+            #     + torch.sum(q0.log_prob(z), dim=-1)
             #     - logjacobin.view(-1)
             # )
             # logjacobin = logjacobin.unsqueeze(-1)
@@ -77,9 +77,9 @@ def train_cvae(run, config, model, classifier, criterion_cla, criterion_pair, cr
             # kl_loss_z = -0.5 * torch.sum(1 + log_var - mean.pow(2) - log_var.exp())
 
 
-            mu_q = theta.mean(0)
-            sigma_q = torch.exp(logjacobin).mean(0)
-            kld_theta = 0.5 * (-sigma_q.log() - 1 + sigma_q + mu_q.pow(2))
+            # mu_q = theta.mean(0)
+            # sigma_q = torch.exp(logjacobin).mean(0)
+            # kld_theta = 0.5 * (-sigma_q.log() - 1 + sigma_q + mu_q.pow(2))
             
             # Ensure sigma_q is positive and non-zero
             # sigma_q = torch.clamp(sigma_q, min=1e-8)
@@ -91,17 +91,24 @@ def train_cvae(run, config, model, classifier, criterion_cla, criterion_pair, cr
             # kld_theta = -torch.logsumexp(log_likelihood, dim=0)
 
 
-            kld_theta = kld_theta.mean()
+            base_dist = Normal(torch.zeros_like(mean), torch.ones_like(log_var))
+            prior = torch.sum(base_dist.log_prob(theta), dim=-1) + logjacobin
+            q0 = Normal(mean, torch.exp(0.5 * log_var))
+            posterior = torch.sum(q0.log_prob(z), dim=-1)
+
+            kld_theta = (posterior - prior).mean()
             kl_loss = kld_theta
 
-
+            # print("posterior: {}, prior: {}".format(posterior.mean(), prior.mean()))
+            print("mean: {}, log_var: {}, z: {}".format(mean.mean(), log_var.mean(), z.mean()))
 
             # bce or mse
             recon_loss = criterion_recon(recon_x, imgs_tensor)
 
             beta = 0.5
             loss = cls_loss  + beta *(kl_loss + kld_theta) + recon_loss
-            # loss = cls_loss  + bce_loss
+
+            # loss = cls_loss + recon_loss
 
         optimizer.zero_grad()
         if config.TRAIN.AMP: