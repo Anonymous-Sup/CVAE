66790d8f8d031f5258e422c2ac5cbf71d598a8e1
GavinYoung1 <162417316+GavinYoung1@users.noreply.github.com>
2024-09-04 22:25:44 +0800

    update2

--- scripts/train_base_msmt17.sh
+++ scripts/train_base_msmt17.sh
@@ -19,7 +19,7 @@ cd /home/zhengwei/github/CVAE
 # > nohup_logs/train_baseMSMT17_SimpleVAE_128+64z_1e3_60+120_KLtotalZ_noCLS.log 2>&1 &
 
 
-CUDA_VISIBLE_DEVICES=1
+CUDA_VISIBLE_DEVICES=2
 # ===========Base ReID+CLS Stage traning================
 nohup python -u main.py --cfg ./configs/base_msmt17/clipreid_cvae_kl.yaml \
 --root /home/zhengwei/my_data/datasets \
@@ -29,7 +29,7 @@ nohup python -u main.py --cfg ./configs/base_msmt17/clipreid_cvae_kl.yaml \
 --train_format base \
 --train_stage reid+cls_stage \
 --resume  /data/zhengwei/CVAE/outputs/msmt17/clipreid_simplevae_base/2024-08-31/baseMSMT17_SimpleVAE_128+64z_1e3_60+120_KLtotalZ_noCLS \
---gpu 1 \
+--gpu 2 \
 --saved_name baseMSMT17_SimpleVAE_128+64z_1e3_60+120_KLtotalZ_L1280+bn+distCLS_Trip+Center+Ce \
 --vae_type SinpleVAE \
 --recon_loss mse \
@@ -46,7 +46,7 @@ nohup python -u main.py --cfg ./configs/base_msmt17/clipreid_cvae_kl.yaml \
 --train_format base \
 --train_stage reid+cls_stage \
 --resume  /data/zhengwei/CVAE/outputs/msmt17/clipreid_simplevae_base/2024-08-31/baseMSMT17_SimpleVAE+2E_128+64z_1e3_60+120_KLtotalZ_noCLS \
---gpu 1 \
+--gpu 2 \
 --saved_name baseMSMT17_SimpleVAE+2E_128+64z_1e3_60+120_KLtotalZ_L1280+bn+distCLS_Trip+Center+Ce \
 --vae_type SinpleVAE \
 --recon_loss mse \
