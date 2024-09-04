7b95eaf710cdb2462e2460558efef046e4229c74
GavinYoung1 <162417316+GavinYoung1@users.noreply.github.com>
2024-09-04 13:28:04 +0800

    cls + trip script

--- scripts/train_novel_mask1k_basedmsmt17.sh
+++ scripts/train_novel_mask1k_basedmsmt17.sh
@@ -46,7 +46,7 @@ cd /home/zhengwei/github/CVAE
 # --gaussian MultivariateNormal \
 # > nohup_logs/tune_novelMask1K_SimpleVAE+2E_128+64z_1e3_60+120_KLtotalZ_noCLS.log 2>&1 &
 
-CUDA_VISIBLE_DEVICES=2
+CUDA_VISIBLE_DEVICES=3
 # ===========Base ReID+CLS Stage traning================
 nohup python -u main.py --cfg ./configs/clipreid_cvae_stage2.yaml \
 --root /home/zhengwei/my_data/datasets \
@@ -56,13 +56,13 @@ nohup python -u main.py --cfg ./configs/clipreid_cvae_stage2.yaml \
 --train_format novel \
 --train_stage reid+cls_stage \
 --resume  /data/zhengwei/CVAE/outputs/market1k/clipreid_tuned_person/novel/2024-09-04/novelMask1K_SimpleVAE_128+64z_1e3_60+120_KLtotalZ_noCLS \
---gpu 2 \
---saved_name novelMask1K_SimpleVAE_128+64z_1e3_60+120_KLtotalZ_L1280+distCLS_Trip+Center+Ce \
+--gpu 3 \
+--saved_name novelMask1K_SimpleVAE_128+64z_1e3_60+120_KLtotalZ_L1280+CLS_Trip+Center+Ce \
 --vae_type SinpleVAE \
 --recon_loss mse \
 --reid_loss crossentropy \
 --gaussian MultivariateNormal \
-> nohup_logs/tune_2nd_novelMask1K_SimpleVAE_128+64z_1e3_60+120_KLtotalZ_L1280+distCLS_Trip+Center+Ce.log 2>&1 &
+> nohup_logs/tune_2nd_novelMask1K_SimpleVAE_128+64z_1e3_60+120_KLtotalZ_L1280+CLS_Trip+Center+Ce.log 2>&1 &
 
 
 nohup python -u main.py --cfg ./configs/clipreid_cvae_stage2.yaml \
@@ -73,12 +73,12 @@ nohup python -u main.py --cfg ./configs/clipreid_cvae_stage2.yaml \
 --train_format novel \
 --train_stage reid+cls_stage \
 --resume  /data/zhengwei/CVAE/outputs/market1k/clipreid_tuned_person/novel/2024-09-04/novelMask1K_SimpleVAE+2E_128+64z_1e3_60+120_KLtotalZ_noCLS \
---gpu 2 \
---saved_name novelMask1K_SimpleVAE+2E_128+64z_1e3_60+120_KLtotalZ_L1280+distCLS_Trip+Center+Ce \
+--gpu 3 \
+--saved_name novelMask1K_SimpleVAE+2E_128+64z_1e3_60+120_KLtotalZ_L1280+CLS_Trip+Center+Ce \
 --vae_type SinpleVAE \
 --recon_loss mse \
 --use_two_encoder \
 --reid_loss crossentropy \
 --gaussian MultivariateNormal \
-> nohup_logs/tune_2nd_novelMask1K_SimpleVAE+2E_128+64z_1e3_60+120_KLtotalZ_L1280+distCLS_Trip+Center+Ce.log 2>&1 &
+> nohup_logs/tune_2nd_novelMask1K_SimpleVAE+2E_128+64z_1e3_60+120_KLtotalZ_L1280+CLS_Trip+Center+Ce.log 2>&1 &
 
