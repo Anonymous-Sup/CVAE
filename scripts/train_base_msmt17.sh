243771ab9c3630057e6b89f925c2c15a863f2935
GavinYoung1 <162417316+GavinYoung1@users.noreply.github.com>
2024-09-05 11:26:27 +0800

    scripts

--- scripts/train_base_msmt17.sh
+++ scripts/train_base_msmt17.sh
@@ -30,12 +30,12 @@ nohup python -u main.py --cfg ./configs/base_msmt17/clipreid_cvae_kl.yaml \
 --train_stage reid+cls_stage \
 --resume  /data/zhengwei/CVAE/outputs/msmt17/clipreid_simplevae_base/2024-08-31/baseMSMT17_SimpleVAE_128+64z_1e3_60+120_KLtotalZ_noCLS \
 --gpu 2 \
---saved_name baseMSMT17_SimpleVAE_128+64z_1e3_60+120_KLtotalZ_L1280+bn+distCLS_Trip+Center+Ce \
+--saved_name baseMSMT17_SimpleVAE_128+64z_1e3_60+120_KLtotalZ_L1280+bnTuneVersion+distCLS_Trip+Center+Ce \
 --vae_type SinpleVAE \
 --recon_loss mse \
 --reid_loss crossentropy \
 --gaussian MultivariateNormal \
-> nohup_logs/train_2nd_baseMSMT17_SimpleVAE_128+64z_1e3_60+120_KLtotalZ_L1280+bn+distCLS_Trip+Center+Ce.log 2>&1 &
+> nohup_logs/train_2nd_baseMSMT17_SimpleVAE_128+64z_1e3_60+120_KLtotalZ_L1280+bnTuneVersion+distCLS_Trip+Center+Ce.log 2>&1 &
 
 
 nohup python -u main.py --cfg ./configs/base_msmt17/clipreid_cvae_kl.yaml \
@@ -47,13 +47,13 @@ nohup python -u main.py --cfg ./configs/base_msmt17/clipreid_cvae_kl.yaml \
 --train_stage reid+cls_stage \
 --resume  /data/zhengwei/CVAE/outputs/msmt17/clipreid_simplevae_base/2024-08-31/baseMSMT17_SimpleVAE+2E_128+64z_1e3_60+120_KLtotalZ_noCLS \
 --gpu 2 \
---saved_name baseMSMT17_SimpleVAE+2E_128+64z_1e3_60+120_KLtotalZ_L1280+bn+distCLS_Trip+Center+Ce \
+--saved_name baseMSMT17_SimpleVAE+2E_128+64z_1e3_60+120_KLtotalZ_L1280+bnTuneVersion+distCLS_Trip+Center+Ce \
 --vae_type SinpleVAE \
 --recon_loss mse \
 --use_two_encoder \
 --reid_loss crossentropy \
 --gaussian MultivariateNormal \
-> nohup_logs/train_2nd_baseMSMT17_SimpleVAE+2E_128+64z_1e3_60+120_KLtotalZ_L1280+bn+distCLS_Trip+Center+Ce.log 2>&1 &
+> nohup_logs/train_2nd_baseMSMT17_SimpleVAE+2E_128+64z_1e3_60+120_KLtotalZ_L1280+bnTuneVersion+distCLS_Trip+Center+Ce.log 2>&1 &
 
 # # ===========Base CLS Stage traning================
 # nohup python -u main.py --cfg ./configs/base_msmt17/clipreid_cvae_kl.yaml \
