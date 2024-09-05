ab252ae2e753f35e91daea51937c1b7bb8ab99dc
GavinYoung1 <162417316+GavinYoung1@users.noreply.github.com>
2024-09-05 23:06:44 +0800

    update training script 2

--- scripts/train_novel_mask1k_basedmsmt17.sh
+++ scripts/train_novel_mask1k_basedmsmt17.sh
@@ -46,7 +46,7 @@ cd /home/zhengwei/github/CVAE
 # --gaussian MultivariateNormal \
 # > nohup_logs/tune_novelMask1K_SimpleVAE+2E_128+64z_1e3_60+120_KLtotalZ_noCLS_forLinear.log 2>&1 &
 
-CUDA_VISIBLE_DEVICES=1
+CUDA_VISIBLE_DEVICES=2
 # ===========Base ReID+CLS Stage traning================
 nohup python -u main.py --cfg ./configs/clipreid_cvae_stage2.yaml \
 --root /home/zhengwei/my_data/datasets \
@@ -55,14 +55,14 @@ nohup python -u main.py --cfg ./configs/clipreid_cvae_stage2.yaml \
 --format_tag tensor \
 --train_format novel \
 --train_stage reid+cls_stage \
---resume  /data/zhengwei/CVAE/outputs/market1k/clipreid_tuned_person/novel/2024-09-05/novelMask1K_SimpleVAE_128+64z_1e3_60+120_KLtotalZ_noCLS \
---gpu 1 \
---saved_name novelMask1K_SimpleVAE_128+64z_1e3_60+120_KLtotalZ_L1280+bnTuneVersion+distCLS_Trip+Center+Ce \
+--resume  /data/zhengwei/CVAE/outputs/market1k/clipreid_tuned_person/novel/2024-09-05/novelMask1K_SimpleVAE_128+64z_1e3_60+120_KLtotalZ_noCLS_forLinear \
+--gpu 2 \
+--saved_name novelMask1K_SimpleVAE_128+64z_1e3_60+120_KLtotalZ_L1280+bnTuneVersion+CLS_Trip+Center+Ce \
 --vae_type SinpleVAE \
 --recon_loss mse \
 --reid_loss crossentropy \
 --gaussian MultivariateNormal \
-> nohup_logs/tune_2nd_novelMask1K_SimpleVAE_128+64z_1e3_60+120_KLtotalZ_L1280+bnTuneVersion+distCLS_Trip+Center+Ce.log 2>&1 &
+> nohup_logs/tune_2nd_novelMask1K_SimpleVAE_128+64z_1e3_60+120_KLtotalZ_L1280+bnTuneVersion+CLS_Trip+Center+Ce.log 2>&1 &
 
 
 nohup python -u main.py --cfg ./configs/clipreid_cvae_stage2.yaml \
@@ -72,13 +72,13 @@ nohup python -u main.py --cfg ./configs/clipreid_cvae_stage2.yaml \
 --format_tag tensor \
 --train_format novel \
 --train_stage reid+cls_stage \
---resume  /data/zhengwei/CVAE/outputs/market1k/clipreid_tuned_person/novel/2024-09-05/novelMask1K_SimpleVAE+2E_128+64z_1e3_60+120_KLtotalZ_noCLS \
---gpu 1 \
---saved_name novelMask1K_SimpleVAE+2E_128+64z_1e3_60+120_KLtotalZ_L1280+bnTuneVersion+distCLS_Trip+Center+Ce \
+--resume  /data/zhengwei/CVAE/outputs/market1k/clipreid_tuned_person/novel/2024-09-05/novelMask1K_SimpleVAE+2E_128+64z_1e3_60+120_KLtotalZ_noCLS_forLinear \
+--gpu 2 \
+--saved_name novelMask1K_SimpleVAE+2E_128+64z_1e3_60+120_KLtotalZ_L1280+bnTuneVersion+CLS_Trip+Center+Ce \
 --vae_type SinpleVAE \
 --recon_loss mse \
 --use_two_encoder \
 --reid_loss crossentropy \
 --gaussian MultivariateNormal \
-> nohup_logs/tune_2nd_novelMask1K_SimpleVAE+2E_128+64z_1e3_60+120_KLtotalZ_L1280+bnTuneVersion+distCLS_Trip+Center+Ce.log 2>&1 &
+> nohup_logs/tune_2nd_novelMask1K_SimpleVAE+2E_128+64z_1e3_60+120_KLtotalZ_L1280+bnTuneVersion+CLS_Trip+Center+Ce.log 2>&1 &
 
