79a4b307fda12c790a24e83fc35ce5418e75cd1b
GavinYoung1 <162417316+GavinYoung1@users.noreply.github.com>
2024-09-18 16:01:23 +0800

    update true 5w5s 1st train

--- scripts/train_novel_sketchy.sh
+++ scripts/train_novel_sketchy.sh
@@ -10,7 +10,7 @@ cd /home/zhengwei/github/CVAE
 # --use_two_encoder  # for model
 # --use_NCE # for loss
 
-CUDA_VISIBLE_DEVICES=1
+CUDA_VISIBLE_DEVICES=3
 # # ===========Stage 1 traning================
 nohup python -u main.py --cfg ./configs/sketchy_categray/clipreid_cvae_kl_novel_true5wKs.yaml \
 --root /home/zhengwei/my_data/datasets \
@@ -20,13 +20,13 @@ nohup python -u main.py --cfg ./configs/sketchy_categray/clipreid_cvae_kl_novel_
 --train_format novel \
 --resume /data/zhengwei/CVAE/outputs/sketchy/clipreid_simplevae_base/2024-09-17/baseSketchy_SimpleVAE_128+64z_1e3_60+120_KLtotalZ_noCLS/2nd_stage/2024-09-17/baseSketchy_SimpleVAE_128+64z_1e3_60+120_KLtotalZ_L1280+bnTuneVersion+CLS_Trip+Center+Ce \
 --train_stage klNocls_stage \
---gpu 1 \
---saved_name novelSketchy_SimpleVAE_128+64z_1e3_60+120_KLtotalZ_True5w1s_noCLS_forLinear \
+--gpu 3 \
+--saved_name novelSketchy_SimpleVAE_128+64z_1e3_60+120_KLtotalZ_True5w5s_noCLS_forLinear \
 --vae_type SinpleVAE \
 --recon_loss mse \
 --reid_loss crossentropy \
 --gaussian MultivariateNormal \
-> nohup_logs/tune_novelSketchy_SimpleVAE_128+64z_1e3_60+120_KLtotalZ_True5w1s_noCLS_forLinear.log 2>&1 &
+> nohup_logs/tune_novelSketchy_SimpleVAE_128+64z_1e3_60+120_KLtotalZ_True5w5s_noCLS_forLinear.log 2>&1 &
 
 # # ===========Stage 1 traning================
 nohup python -u main.py --cfg ./configs/sketchy_categray/clipreid_cvae_kl_novel_true5wKs.yaml \
@@ -37,14 +37,14 @@ nohup python -u main.py --cfg ./configs/sketchy_categray/clipreid_cvae_kl_novel_
 --train_format novel \
 --resume /data/zhengwei/CVAE/outputs/sketchy/clipreid_simplevae_base/2024-09-17/baseSketchy_SimpleVAE+2E_128+64z_1e3_60+120_KLtotalZ_noCLS/2nd_stage/2024-09-18/baseSketchy_SimpleVAE+2E_128+64z_1e3_60+120_KLtotalZ_L1280+bnTuneVersion+CLS_Trip+Center+Ce \
 --train_stage klNocls_stage \
---gpu 1 \
---saved_name novelSketchy_SimpleVAE+2E_128+64z_1e3_60+120_KLtotalZ_True5w1s_noCLS_forLinear \
+--gpu 3 \
+--saved_name novelSketchy_SimpleVAE+2E_128+64z_1e3_60+120_KLtotalZ_True5w5s_noCLS_forLinear \
 --vae_type SinpleVAE \
 --recon_loss mse \
 --use_two_encoder \
 --reid_loss crossentropy \
 --gaussian MultivariateNormal \
-> nohup_logs/tune_novelSketchy_SimpleVAE+2E_128+64z_1e3_60+120_KLtotalZ_True5w1s_noCLS_forLinear.log 2>&1 &
+> nohup_logs/tune_novelSketchy_SimpleVAE+2E_128+64z_1e3_60+120_KLtotalZ_True5w5s_noCLS_forLinear.log 2>&1 &
 
 # CUDA_VISIBLE_DEVICES=1
 # # ===========Base ReID+CLS Stage traning================
