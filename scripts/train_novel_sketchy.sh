2e26c2c5949453428af0784a915c7eb0e4421212
GavinYoung1 <162417316+GavinYoung1@users.noreply.github.com>
2024-09-18 16:43:21 +0800

    true5w1s

--- scripts/train_novel_sketchy.sh
+++ scripts/train_novel_sketchy.sh
@@ -46,7 +46,7 @@ cd /home/zhengwei/github/CVAE
 # --gaussian MultivariateNormal \
 # > nohup_logs/tune_novelSketchy_SimpleVAE+2E_128+64z_1e3_60+120_KLtotalZ_True5w5s_noCLS_forLinear.log 2>&1 &
 
-CUDA_VISIBLE_DEVICES=1
+CUDA_VISIBLE_DEVICES=2
 # ===========Base ReID+CLS Stage traning================
 nohup python -u main.py --cfg ./configs/sketchy_categray/clipreid_cvae_kl_novel_true5wKs.yaml \
 --root /home/zhengwei/my_data/datasets \
@@ -55,14 +55,14 @@ nohup python -u main.py --cfg ./configs/sketchy_categray/clipreid_cvae_kl_novel_
 --format_tag tensor \
 --train_format novel \
 --train_stage reid+cls_stage \
---resume  /data/zhengwei/CVAE/outputs/sketchy/clipreid_simplevae_base/novel/2024-09-18/novelSketchy_SimpleVAE_128+64z_1e3_60+120_KLtotalZ_True5w5s_noCLS_forLinear \
---gpu 1 \
---saved_name novelSketchy_SimpleVAE_128+64z_1e4_60+120_KLtotalZ_True5w5s_L1280+bnTuneVersion+CLS_Trip+Center+Ce \
+--resume  /data/zhengwei/CVAE/outputs/sketchy/clipreid_simplevae_base/novel/2024-09-18/novelSketchy_SimpleVAE_128+64z_1e3_60+120_KLtotalZ_True5w1s_noCLS_forLinear \
+--gpu 2 \
+--saved_name novelSketchy_SimpleVAE_128+64z_1e4_60+120_KLtotalZ_True5w1s_L1280+bnTuneVersion+CLS_Trip+Center+Ce \
 --vae_type SinpleVAE \
 --recon_loss mse \
 --reid_loss crossentropy \
 --gaussian MultivariateNormal \
-> nohup_logs/tune_2nd_novelSketchy_SimpleVAE_128+64z_1e4_60+120_KLtotalZ_True5w5s_L1280+bnTuneVersion+CLS_Trip+Center+Ce.log 2>&1 &
+> nohup_logs/tune_2nd_novelSketchy_SimpleVAE_128+64z_1e4_60+120_KLtotalZ_True5w1s_L1280+bnTuneVersion+CLS_Trip+Center+Ce.log 2>&1 &
 
 
 nohup python -u main.py --cfg ./configs/sketchy_categray/clipreid_cvae_kl_novel_true5wKs.yaml \
@@ -72,13 +72,13 @@ nohup python -u main.py --cfg ./configs/sketchy_categray/clipreid_cvae_kl_novel_
 --format_tag tensor \
 --train_format novel \
 --train_stage reid+cls_stage \
---resume  /data/zhengwei/CVAE/outputs/sketchy/clipreid_simplevae_base/novel/2024-09-18/novelSketchy_SimpleVAE+2E_128+64z_1e3_60+120_KLtotalZ_True5w5s_noCLS_forLinear \
---gpu 1 \
---saved_name novelSketchy_SimpleVAE+2E_128+64z_1e4_60+120_KLtotalZ_True5w5s_L1280+bnTuneVersion+CLS_Trip+Center+Ce \
+--resume  /data/zhengwei/CVAE/outputs/sketchy/clipreid_simplevae_base/novel/2024-09-18/novelSketchy_SimpleVAE+2E_128+64z_1e3_60+120_KLtotalZ_True5w1s_noCLS_forLinear \
+--gpu 2 \
+--saved_name novelSketchy_SimpleVAE+2E_128+64z_1e4_60+120_KLtotalZ_True5w1s_L1280+bnTuneVersion+CLS_Trip+Center+Ce \
 --vae_type SinpleVAE \
 --recon_loss mse \
 --use_two_encoder \
 --reid_loss crossentropy \
 --gaussian MultivariateNormal \
-> nohup_logs/tune_2nd_novelSketchy_SimpleVAE+2E_128+64z_1e4_60+120_KLtotalZ_True5w5s_L1280+bnTuneVersion+CLS_Trip+Center+Ce.log 2>&1 &
+> nohup_logs/tune_2nd_novelSketchy_SimpleVAE+2E_128+64z_1e4_60+120_KLtotalZ_True5w1s_L1280+bnTuneVersion+CLS_Trip+Center+Ce.log 2>&1 &
 
