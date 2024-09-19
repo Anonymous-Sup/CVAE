df1bceaa9a69c6b021cbac826a28092e605a7e1a
GavinYoung1 <162417316+GavinYoung1@users.noreply.github.com>
2024-09-19 10:07:04 +0800

    update novel  true5w1s 1st

--- scripts/train_novel_tuberlin.sh
+++ scripts/train_novel_tuberlin.sh
@@ -12,7 +12,7 @@ cd /home/zhengwei/github/CVAE
 
 CUDA_VISIBLE_DEVICES=1
 # # ===========Stage 1 traning================
-nohup python -u main.py --cfg ./configs/tuberlin_categray/clipreid_cvae_kl_novel.yaml \
+nohup python -u main.py --cfg ./configs/tuberlin_categray/clipreid_cvae_kl_novel_true5wKs.yaml \
 --root /home/zhengwei/my_data/datasets \
 --output /data/zhengwei/CVAE/outputs \
 --dataset tuberlin \
@@ -20,16 +20,16 @@ nohup python -u main.py --cfg ./configs/tuberlin_categray/clipreid_cvae_kl_novel
 --train_format novel \
 --resume /data/zhengwei/CVAE/outputs/tuberlin/clipreid_simplevae_base/2024-09-18/baseTuberlin_SimpleVAE_128+64z_1e3_60+120_KLtotalZ_noCLS/2nd_stage/2024-09-18/baseTUBerlin_SimpleVAE_128+64z_1e4_60+120_KLtotalZ_L1280+bnTuneVersion+CLS_Trip+Center+Ce \
 --train_stage klNocls_stage \
---gpu 3 \
---saved_name novelTUBerlin_SimpleVAE_128+64z_1e3_60+120_KLtotalZ+lessC_all5w5s_noCLS_forLinear \
+--gpu 1 \
+--saved_name novelTUBerlin_SimpleVAE_128+64z_1e3_60+120_KLtotalZ+lessC_true5w1s_noCLS_forLinear \
 --vae_type SinpleVAE \
 --recon_loss mse \
 --reid_loss crossentropy \
 --gaussian MultivariateNormal \
-> nohup_logs/tune_novelTUBerlin_SimpleVAE_128+64z_1e3_60+120_KLtotalZ+lessC_all5w5s_noCLS_forLinear.log 2>&1 &
+> nohup_logs/tune_novelTUBerlin_SimpleVAE_128+64z_1e3_60+120_KLtotalZ+lessC_true5w1s_noCLS_forLinear.log 2>&1 &
 
 # # ===========Stage 1 traning================
-nohup python -u main.py --cfg ./configs/tuberlin_categray/clipreid_cvae_kl_novel.yaml \
+nohup python -u main.py --cfg ./configs/tuberlin_categray/clipreid_cvae_kl_novel_true5wKs.yaml \
 --root /home/zhengwei/my_data/datasets \
 --output /data/zhengwei/CVAE/outputs \
 --dataset tuberlin \
@@ -37,14 +37,14 @@ nohup python -u main.py --cfg ./configs/tuberlin_categray/clipreid_cvae_kl_novel
 --train_format novel \
 --resume /data/zhengwei/CVAE/outputs/tuberlin/clipreid_simplevae_base/2024-09-18/baseTuberlin_SimpleVAE+2E_128+64z_1e3_60+120_KLtotalZ_noCLS/2nd_stage/2024-09-18/baseTUBerlin_SimpleVAE+2E_128+64z_1e4_60+120_KLtotalZ_L1280+bnTuneVersion+CLS_Trip+Center+Ce \
 --train_stage klNocls_stage \
---gpu 3 \
---saved_name novelTUBerlin_SimpleVAE+2E_128+64z_1e3_60+120_KLtotalZ+lessC_all5w5s_noCLS_forLinear \
+--gpu 1 \
+--saved_name novelTUBerlin_SimpleVAE+2E_128+64z_1e3_60+120_KLtotalZ+lessC_true5w1s_noCLS_forLinear \
 --vae_type SinpleVAE \
 --recon_loss mse \
 --use_two_encoder \
 --reid_loss crossentropy \
 --gaussian MultivariateNormal \
-> nohup_logs/tune_novelTUBerlin_SimpleVAE+2E_128+64z_1e3_60+120_KLtotalZ+lessC_all5w5s_noCLS_forLinear.log 2>&1 &
+> nohup_logs/tune_novelTUBerlin_SimpleVAE+2E_128+64z_1e3_60+120_KLtotalZ+lessC_true5w1s_noCLS_forLinear.log 2>&1 &
 
 # CUDA_VISIBLE_DEVICES=3
 # # ===========Base ReID+CLS Stage traning================
