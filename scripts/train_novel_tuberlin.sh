80d416ad9d1484143437e1de835da8446990a0d2
GavinYoung1 <162417316+GavinYoung1@users.noreply.github.com>
2024-09-19 13:30:17 +0800

    update 1e3 for ture 5w5s

--- scripts/train_novel_tuberlin.sh
+++ scripts/train_novel_tuberlin.sh
@@ -57,12 +57,12 @@ nohup python -u main.py --cfg ./configs/tuberlin_categray/clipreid_cvae_kl_novel
 --train_stage reid+cls_stage \
 --resume /data/zhengwei/CVAE/outputs/tuberlin/clipreid_simplevae_base/novel/2024-09-19/novelTUBerlin_SimpleVAE_128+64z_1e3_60+120_KLtotalZ+lessC_all5w5s_noCLS_forLinear \
 --gpu 3 \
---saved_name novelTUBerlin_SimpleVAE_128+64z_1e4_60+120_KLtotalZ+lessC_all5w5s_L1280+bnTuneVersion+CLS_Trip+Center+Ce \
+--saved_name novelTUBerlin_SimpleVAE_128+64z_1e3_60_KLtotalZ+lessC_all5w5s_L1280+bnTuneVersion+CLS_Trip+Center+Ce \
 --vae_type SinpleVAE \
 --recon_loss mse \
 --reid_loss crossentropy \
 --gaussian MultivariateNormal \
-> nohup_logs/tune_2nd_novelTUBerlin_SimpleVAE_128+64z_1e4_60+120_KLtotalZ+lessC_all5w5s_L1280+bnTuneVersion+CLS_Trip+Center+Ce.log 2>&1 &
+> nohup_logs/tune_2nd_novelTUBerlin_SimpleVAE_128+64z_1e3_60_KLtotalZ+lessC_all5w5s_L1280+bnTuneVersion+CLS_Trip+Center+Ce.log 2>&1 &
 
 
 nohup python -u main.py --cfg ./configs/tuberlin_categray/clipreid_cvae_kl_novel.yaml \
@@ -74,11 +74,11 @@ nohup python -u main.py --cfg ./configs/tuberlin_categray/clipreid_cvae_kl_novel
 --train_stage reid+cls_stage \
 --resume  /data/zhengwei/CVAE/outputs/tuberlin/clipreid_simplevae_base/novel/2024-09-19/novelTUBerlin_SimpleVAE+2E_128+64z_1e3_60+120_KLtotalZ+lessC_all5w5s_noCLS_forLinear \
 --gpu 3 \
---saved_name novelTUBerlin_SimpleVAE+2E_128+64z_1e4_60+120_KLtotalZ+lessC_all5w5s_L1280+bnTuneVersion+CLS_Trip+Center+Ce \
+--saved_name novelTUBerlin_SimpleVAE+2E_128+64z_1e3_60_KLtotalZ+lessC_all5w5s_L1280+bnTuneVersion+CLS_Trip+Center+Ce \
 --vae_type SinpleVAE \
 --recon_loss mse \
 --use_two_encoder \
 --reid_loss crossentropy \
 --gaussian MultivariateNormal \
-> nohup_logs/tune_2nd_novelTUBerlin_SimpleVAE+2E_128+64z_1e4_60+120_KLtotalZ+lessC_all5w5s_L1280+bnTuneVersion+CLS_Trip+Center+Ce.log 2>&1 &
+> nohup_logs/tune_2nd_novelTUBerlin_SimpleVAE+2E_128+64z_1e3_60_KLtotalZ+lessC_all5w5s_L1280+bnTuneVersion+CLS_Trip+Center+Ce.log 2>&1 &
 
