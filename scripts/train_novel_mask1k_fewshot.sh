bcada017f9d2ad24daeeeb681a77744b688d397d
GavinYoung1 <162417316+GavinYoung1@users.noreply.github.com>
2024-09-21 20:18:57 +0800

    update all 5w2s

--- scripts/train_novel_mask1k_fewshot.sh
+++ scripts/train_novel_mask1k_fewshot.sh
@@ -10,7 +10,7 @@ cd /home/zhengwei/github/CVAE
 # --use_two_encoder  # for model
 # --use_NCE # for loss
 
-CUDA_VISIBLE_DEVICES=1
+CUDA_VISIBLE_DEVICES=3
 # # ===========Stage 1 traning================
 nohup python -u main.py --cfg ./configs/novel_mask1k/clipreid_cvae_kl_novel.yaml \
 --root /data/zhengwei/datasets \
@@ -20,13 +20,13 @@ nohup python -u main.py --cfg ./configs/novel_mask1k/clipreid_cvae_kl_novel.yaml
 --train_format novel \
 --resume /data/zhengwei/CVAE/outputs/msmt17/clipreid_simplevae_base/2024-08-31/baseMSMT17_SimpleVAE_128+64z_1e3_60+120_KLtotalZ_noCLS/2nd_stage/2024-09-05/baseMSMT17_SimpleVAE_128+64z_1e3_60+120_KLtotalZ_L1280+bnTuneVersion+CLS_Trip+Center+Ce \
 --train_stage klNocls_stage \
---gpu 1 \
---saved_name novelMask1KFew_SimpleVAE_128+64z_1e3_30+60_KLtotalZ+lessC_all5w1s_noCLS_forLinear \
+--gpu 3 \
+--saved_name novelMask1KFew_SimpleVAE_128+64z_1e3_30+60_KLtotalZ+lessC_all5w2s_noCLS_forLinear \
 --vae_type SinpleVAE \
 --recon_loss mse \
 --reid_loss crossentropy \
 --gaussian MultivariateNormal \
-> nohup_logs/tune_novelMask1KFew_SimpleVAE_128+64z_1e3_30+60_KLtotalZ+lessC_all5w1s_noCLS_forLinear.log 2>&1 &
+> nohup_logs/tune_novelMask1KFew_SimpleVAE_128+64z_1e3_30+60_KLtotalZ+lessC_all5w2s_noCLS_forLinear.log 2>&1 &
 
 # # ===========Stage 1 traning================
 nohup python -u main.py --cfg ./configs/novel_mask1k/clipreid_cvae_kl_novel.yaml \
@@ -37,14 +37,14 @@ nohup python -u main.py --cfg ./configs/novel_mask1k/clipreid_cvae_kl_novel.yaml
 --train_format novel \
 --resume /data/zhengwei/CVAE/outputs/msmt17/clipreid_simplevae_base/2024-08-31/baseMSMT17_SimpleVAE+2E_128+64z_1e3_60+120_KLtotalZ_noCLS/2nd_stage/2024-09-05/baseMSMT17_SimpleVAE+2E_128+64z_1e3_60+120_KLtotalZ_L1280+bnTuneVersion+CLS_Trip+Center+Ce \
 --train_stage klNocls_stage \
---gpu 1 \
---saved_name novelMask1KFew_SimpleVAE+2E_128+64z_1e3_30+60_KLtotalZ+lessC_all5w1s_noCLS_forLinear \
+--gpu 3 \
+--saved_name novelMask1KFew_SimpleVAE+2E_128+64z_1e3_30+60_KLtotalZ+lessC_all5w2s_noCLS_forLinear \
 --vae_type SinpleVAE \
 --recon_loss mse \
 --use_two_encoder \
 --reid_loss crossentropy \
 --gaussian MultivariateNormal \
-> nohup_logs/tune_novelMask1KFew_SimpleVAE+2E_128+64z_1e3_30+60_KLtotalZ+lessC_all5w1s_noCLS_forLinear.log 2>&1 &
+> nohup_logs/tune_novelMask1KFew_SimpleVAE+2E_128+64z_1e3_30+60_KLtotalZ+lessC_all5w2s_noCLS_forLinear.log 2>&1 &
 
 
 # CUDA_VISIBLE_DEVICES=2
