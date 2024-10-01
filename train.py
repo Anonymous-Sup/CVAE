6b8d5209edfeab6f830e9f3ac2c0a379900bfb2f
GavinYoung1 <162417316+GavinYoung1@users.noreply.github.com>
2024-10-02 04:37:49 +0800

    update save U

--- train.py
+++ train.py
@@ -9,7 +9,7 @@ import torch.nn.functional as F
 from utils import plot_histogram, plot_pair_seperate, plot_correlation_matrix, plot_scatter_1D, plot_scatter_2D
 from utils import plot_histogram_seperate, print_gradients, plot_scatterNN, plot_epoch_Zdim, idx2onehot
 from tools.drawer import tSNE_plot
-
+from scipy.io import savemat
 
 def train_cvae(run, config, model, classifier, classifer_reid, criterion_cla, criterion_pair, criterion_recon, criterion_center,
               optimizer, optimizer_center, trainloader, epoch, iteration_num):
@@ -205,6 +205,19 @@ def train_cvae(run, config, model, classifier, classifer_reid, criterion_cla, cr
         z_collect = z if batch_idx == 0 else torch.cat((z_collect, z), dim=0)
         x_collect = x_pre if batch_idx == 0 else torch.cat((x_collect, x_pre), dim=0)
         zs_collect = fusez_s if batch_idx == 0 else torch.cat((zs_collect, fusez_s), dim=0)
+        u_collect = domian_feature if batch_idx == 0 else torch.cat((u_collect, domian_feature), dim=0)
+        if epoch==0 or epoch+1 == config.TRAIN.MAX_EPOCH:
+            if batch_idx == len(trainloader)-1:
+                # save the feature to mat, use save mat
+                #     gf, qf = gf.cpu().numpy(), qf.cpu().numpy()
+                #     result = {'gallery_f':gf,'gallery_label':g_pids,'gallery_cam':g_camids, 'gallery_name': g_all_img_path ,'query_f':qf,'query_label':q_pids,'query_cam':q_camids, 'query_name': q_all_img_path}
+                #     scipy.io.savemat(mat_save_path + '/pytorch_result.mat', result)
+                u_collect = u_collect.cpu().numpy()
+                result = {'U_{}'.format(epoch):u_collect}
+                mat_save_path = config.OUTPUT_DIR
+                savemat(mat_save_path + '/U_{}.mat'.format(epoch), result)
+
+                
         # if (epoch+1) % 10 == 0 and batch_idx == len(trainloader)-1:   
         # only for the last epoch
         if epoch+1 == config.TRAIN.MAX_EPOCH and batch_idx == len(trainloader)-1:
